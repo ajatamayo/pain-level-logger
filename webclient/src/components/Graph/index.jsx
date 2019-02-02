c46ec6101bc073be21b7d5ed0d97999a85eaa9a8
ariel tamayo <aj@atamayo.io>
2019-02-02 18:08:08 +0800

    Display no of tweets per month

--- webclient/src/components/Graph/index.jsx
+++ webclient/src/components/Graph/index.jsx
@@ -5,15 +5,15 @@ import { connect } from 'react-redux';
 import tweets from './tweets';
 import './graph.css';
 
-const DATE_FORMAT = 'YYYY-MM-DD';
+const DATE_FORMAT = 'YYYY-MM';
 
 const Graph = () => {
-  const groupedTweets = groupBy(tweets, tweet => tweet.created_at.slice(0, 10));
+  const groupedTweets = groupBy(tweets, tweet => tweet.created_at.slice(0, 7));
   const dates = Object.keys(groupedTweets).sort();
   const firstDate = moment(dates[0], DATE_FORMAT);
   const lastDate = moment(dates[dates.length - 1], DATE_FORMAT);
   const monthCount = moment(lastDate).startOf('month').diff(moment(firstDate).startOf('month'), 'months') + 1;
-  const daysCount = moment(lastDate).diff(moment(firstDate), 'days') + 1;
+  const daysCount = moment(lastDate).diff(moment(firstDate), 'months') + 1;
   return (
     <div id="bar-chart">
       <div className="graph">
@@ -27,20 +27,21 @@ const Graph = () => {
           ))}
         </ul>
         <ul className="y-axis">
+          <li><span>50</span></li>
+          <li><span>40</span></li>
+          <li><span>30</span></li>
           <li><span>20</span></li>
-          <li><span>15</span></li>
           <li><span>10</span></li>
-          <li><span>5</span></li>
           <li><span>0</span></li>
         </ul>
         <div className="bars">
-          {range(1, daysCount + 1).map((i) => {
-            const thisDay = moment(firstDate).add(i, 'days').format(DATE_FORMAT);
+          {range(daysCount).map((i) => {
+            const thisDay = moment(firstDate).add(i, 'months').format(DATE_FORMAT);
             const tweetsThisDay = groupedTweets[thisDay];
             if (tweetsThisDay) {
               return (
                 <div key={i} className="bar-group" style={{ width: `${100 / daysCount}%` }}>
-                  <div className="bar bar-1 stat-1" style={{ height: `${100 * tweetsThisDay.length / 20}%` }}>
+                  <div className="bar bar-1 stat-1" style={{ height: `${100 * tweetsThisDay.length / 50}%` }}>
                     <span>{thisDay}</span>
                   </div>
                 </div>
